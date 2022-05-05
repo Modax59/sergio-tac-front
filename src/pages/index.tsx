4f17a1000b131ab45504fb929ce9211d4b88e02c
Modax59 <61698516+Modax59@users.noreply.github.com>
2022-05-05 11:58:38 +0200

    feat: install react query

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 
+import { QueryClient, QueryClientProvider } from 'react-query';
+
 import Base from '@/layout/base';
 import List from '@/templates/products/list';
 
 const index = () => {
+  const queryClient = new QueryClient();
   return (
-    <Base>
-      <List />
-    </Base>
+    <QueryClientProvider client={queryClient}>
+      <Base>
+        <List />
+      </Base>
+    </QueryClientProvider>
   );
 };
 
