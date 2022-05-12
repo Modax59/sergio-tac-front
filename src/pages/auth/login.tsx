7f8851b777c80e30443754af5c879145541ab8cd
MagmusOfficiel <eddyweber80@gmail.com>
2022-05-12 14:19:08 +0200

    Add Page Login & Register

---
+++ src/pages/auth/login.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import Base from '@/layout/base';
+import Login from '@/templates/login/login';
+
+export default function Connexion() {
+  return (
+    <Base>
+      <Login />
+    </Base>
+  );
+}
---
+++ src/pages/auth/register.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import Base from '@/layout/base';
+import Register from '@/templates/register/register';
+
+export default function Inscription() {
+  return (
+    <Base>
+      <Register />
+    </Base>
+  );
+}
