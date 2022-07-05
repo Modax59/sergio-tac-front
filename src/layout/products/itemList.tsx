74656cc7090a138fe6250ce4c3df97e3a15dd727
Modax59 <61698516+ModaxDev@users.noreply.github.com>
2022-07-05 12:28:18 +0200

    feat: add changes

--- src/layout/products/itemList.tsx
+++ src/layout/products/itemList.tsx
@@ -3,17 +3,19 @@ import React from 'react';
 import { useRouter } from 'next/router';
 
 import routes from '@/utils/routes';
+import env from '@/utils/env';
 
 type Props = {
   key: number;
   product: {
     id: number;
-    name: string;
+    title: string;
     href: string;
     imageSrc: string;
     imageAlt: string;
     price: string;
     color: string;
+    category: string;
   };
 };
 
@@ -27,7 +29,7 @@ const ProductItemList = (props: Props) => {
     >
       <div className="lg:aspect-none w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:h-80">
         <img
-          src={props.product.imageSrc}
+          src={env.url2 + props.product.imageSrc}
           alt={props.product.imageAlt}
           className="h-full w-full object-cover object-center lg:h-full lg:w-full"
         />
@@ -35,12 +37,13 @@ const ProductItemList = (props: Props) => {
       <div className="mt-4 flex justify-between">
         <div>
           <h3 className="text-sm text-gray-700">
-            <a href={props.product.href}>
+
+            <button onClick={() => router.push(routes.product_detail+props.product.id)}>
               <span aria-hidden="true" className="absolute inset-0" />
-              {props.product.name}
-            </a>
+              {props.product.title}
+            </button>
           </h3>
-          <p className="mt-1 text-sm text-gray-500">{props.product.color}</p>
+          <p className="mt-1 text-sm text-gray-500">{props.product.category}</p>
         </div>
         <p className="text-sm font-medium text-gray-900">
           {props.product.price}
--- src/templates/products/list.tsx
+++ src/templates/products/list.tsx
@@ -1,40 +1,29 @@
 import ProductItemList from '@/layout/products/itemList';
-
-const products = [
-  {
-    id: 1,
-    name: 'Basic Tee',
-    href: '#',
-    imageSrc:
-      'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg',
-    imageAlt: "Front of men's Basic Tee in black.",
-    price: '$35',
-    color: 'Black',
-  },
-  {
-    id: 2,
-    name: 'Basic Tee',
-    href: '#',
-    imageSrc:
-      'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg',
-    imageAlt: "Front of men's Basic Tee in black.",
-    price: '$35',
-    color: 'Black',
-  },
-  // More products...
-];
+import { useProductsQuery } from '@/hooks/api/useProductsQuery';
 
 export default function List() {
+  const { data } = useProductsQuery();
+
+  console.log(data.data[0].attributes.Categories.data.attributes.name);
+  // @ts-ignore
   return (
-    <div className="bg-white">
-      <div className="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
-        <h2 className="text-2xl font-extrabold tracking-tight text-gray-900">
+    <div className='bg-white'>
+      <div className='mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8'>
+        <h2 className='text-2xl font-extrabold tracking-tight text-gray-900'>
           Customers also purchased
         </h2>
-        <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-          {products.map((product) => (
-            <ProductItemList key={product.id} product={product} />
-          ))}
+        <div className='mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8'>
+          {data &&
+            data.data.map((product) : any => (
+              <ProductItemList key={product.id} product={{
+                id: product.id,
+                title: product.attributes.Title,
+                imageSrc: product.attributes.Thumbnail?.data?.attributes?.url,
+                imageAlt: product.attributes.Title,
+                price: product.attributes.Price.toString(),
+                category: product.attributes.Categories.data.attributes.name,
+              }} />
+            ))}
         </div>
       </div>
     </div>
