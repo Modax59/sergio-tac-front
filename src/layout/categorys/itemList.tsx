83c9fdc2183bf0fb23f81df68f4b28478ac713ef
Modax59 <61698516+ModaxDev@users.noreply.github.com>
2022-07-23 13:57:09 +0200

    feat: add changes

--- src/layout/categorys/itemList.tsx
+++ src/layout/categorys/itemList.tsx
@@ -20,7 +20,7 @@ const CategoryItemList = (props: Props) => {
   return (
     <div
       key={props.category.id}
-      onClick={() => router.push(routes.category_detail + props.category.id)}
+      onClick={() => router.push(routes.products_category+"Homme")}
       className="group relative"
     >
       <div className="lg:aspect-none relative w-full overflow-hidden  bg-gray-200 text-center group-hover:opacity-90">
@@ -32,10 +32,10 @@ const CategoryItemList = (props: Props) => {
 
         <button className="absolute bottom-0 left-1/2 mb-6 w-3/4 -translate-x-1/2 rounded-lg bg-greenst">
           <h3 className="p-1.5 px-5 text-base font-bold text-blackst">
-            <a href={props.category.href}>
+            <button >
               <span aria-hidden="true" className="absolute inset-0 " />
               {props.category.name}
-            </a>
+            </button>
           </h3>
         </button>
       </div>
